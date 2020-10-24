5aa571c8c0d19db5e8ad1b8519244956810acca0
Safwan Chowdhury <safwanrija@yahoo.co.uk>
2020-10-24 16:32:10 +0100

    Initial Upload of files to master

---
+++ cmake-build-debug/CMakeFiles/Coursework_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coursework_2.dir/tic.c.obj"
+  "Coursework_2.exe"
+  "Coursework_2.exe.manifest"
+  "Coursework_2.pdb"
+  "libCoursework_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Coursework_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
